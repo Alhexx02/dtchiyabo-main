336733108fae40c3801cd40374a7778e990be3f7
Alhexx02 <103387346+Alhexx02@users.noreply.github.com>
2022-05-27 01:38:00 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "2f5b35e5872ddbfe1d4a7885f241044c"
-api_id = 9514816
-bot_token = "5380334444:AAH6QagLATPUzdww6q_BIq6Uyg5BfcnO0jg"
-bot_admin_group = -1088031964
+api_hash = "c38c42f5cf7eda2c68444fd877e20300"
+api_id = 19811436
+bot_token = "5133962420:AAG_NSTAti9JWhiYNJnW6yJO54VRAEiZPNU"
+bot_admin_group = -1001558079828
